448aa570e1cdfddae3f42af3f82128c5d4e98b32
Harry EMARH <emarh.harry.code@gmail.com>
2025-07-30 22:42:51 +0200

    Connexion

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -14,37 +14,42 @@ export default function Page() {
             <div className="flex">
                 <div className="max-w-[400px] w-full p-6 bg-white dark:bg-slate-900 shadow-md shadow-slate-100 dark:shadow-slate-800 rounded-md">
                     <Link href="/"><Image src='/images/logo-icon-64.png' width={64} height={64} alt=""/></Link>
-                    <h5 className="my-6 text-xl font-semibold">Signup</h5>
+                    <h5 className="my-6 text-xl font-semibold">Inscription</h5>
                     <form className="text-start">
                         <div className="grid grid-cols-1">
                             <div className="mb-4">
-                                <label className="font-semibold" htmlFor="RegisterName">Your Name:</label>
+                                <label className="font-semibold" htmlFor="RegisterName">Votre nom :</label>
+                                <input id="RegisterName" type="text" className="w-full py-2 px-3 border border-slate-100 dark:border-slate-800 focus:border-violet-600/30 dark:focus:border-violet-600/30 bg-transparent focus:outline-none rounded-md h-10 mt-3" placeholder="MacCode"/>
+                            </div>
+
+                            <div className="mb-4">
+                                <label className="font-semibold" htmlFor="RegisterName">Votre prénom :</label>
                                 <input id="RegisterName" type="text" className="w-full py-2 px-3 border border-slate-100 dark:border-slate-800 focus:border-violet-600/30 dark:focus:border-violet-600/30 bg-transparent focus:outline-none rounded-md h-10 mt-3" placeholder="Harry"/>
                             </div>
 
                             <div className="mb-4">
-                                <label className="font-semibold" htmlFor="LoginEmail">Email Address:</label>
+                                <label className="font-semibold" htmlFor="LoginEmail">Email :</label>
                                 <input id="LoginEmail" type="email" className="w-full py-2 px-3 border border-slate-100 dark:border-slate-800 focus:border-violet-600/30 dark:focus:border-violet-600/30 bg-transparent focus:outline-none rounded-md h-10 mt-3" placeholder="name@example.com"/>
                             </div>
 
                             <div className="mb-4">
-                                <label className="font-semibold" htmlFor="LoginPassword">Password:</label>
+                                <label className="font-semibold" htmlFor="LoginPassword">Mot de passe :</label>
                                 <input id="LoginPassword" type="password" className="w-full py-2 px-3 border border-slate-100 dark:border-slate-800 focus:border-violet-600/30 dark:focus:border-violet-600/30 bg-transparent focus:outline-none rounded-md h-10 mt-3" placeholder="Password:"/>
                             </div>
 
                             <div className="mb-4">
                                 <div className="flex items-center mb-0">
                                     <input className="form-checkbox rounded border-gray-200 dark:border-gray-800 text-violet-600 focus:border-violet-300 focus:ring focus:ring-offset-0 focus:ring-violet-200 focus:ring-opacity-50 me-2" type="checkbox" value="" id="AcceptT&C"/>
-                                    <label className="form-check-label text-slate-400" htmlFor="AcceptT&C">I Accept <Link href="" className="text-violet-600">Terms And Condition</Link></label>
+                                    <label className="form-check-label text-slate-400" htmlFor="AcceptT&C">J'accepte <Link href="" className="text-violet-600">les Termes les Conditions</Link></label>
                                 </div>
                             </div>
 
                             <div className="mb-4">
-                                <Link href="" className="h-10 px-5 tracking-wide inline-flex items-center justify-center font-medium rounded-md bg-violet-600 text-white w-full">Register</Link>
+                                <Link href="" className="h-10 px-5 tracking-wide inline-flex items-center justify-center font-medium rounded-md bg-violet-600 text-white w-full">Inscription</Link>
                             </div>
 
                             <div className="text-center">
-                                <span className="text-slate-400 me-2">Already have an account ? </span> <Link href="/login" className="text-black dark:text-white font-bold">Sign in</Link>
+                                <span className="text-slate-400 me-2">Already have an account ? </span> <Link href="/login" className="text-black dark:text-white font-bold">Connexion</Link>
                             </div>
                         </div>
                     </form>
